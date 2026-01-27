25080559d6bbf1cee6acc0624cd3b8f0f9bace35
Mark McCarthy <mccarthy17mark@gmail.com>
2026-01-26 21:50:58 -0500

    feat: Add next piece window

--- godot/game.gd
+++ godot/game.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 
+var game_active = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$N_tris_label.text = str(Global.N) + "-tris" 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("StartGame"):
+		$StartPrompt.hide()
+		game_active = true
 
 func _on_menu_button_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
